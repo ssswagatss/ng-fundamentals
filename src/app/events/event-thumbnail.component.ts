88f682496dd11407f82bb88448300cded9af480b
Swagat Swain <swagat.swain@mindfiresolutions.com>
2018-08-28 17:09:59 +0530

    ng class

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,6 +5,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     templateUrl: 'event-thumbnail.component.html',
     styles:[
         `
+        .green {color : #003300 !important;}
+        .bold {font-weight : bold;}
         .thumbnail {min-height : 210px;}
         .well div { color:#bbb}
         `
@@ -14,4 +16,16 @@ export class EventThumbnailComponent {
     constructor() { }
     @Input() event : any;
     
+
+    getEventTimeClass(){
+        const isEarly : boolean = this.event && this.event.time=='8:00 am';
+        if(isEarly)
+        {
+            // return "green bold";
+            // return {green : isEarly , bold:isEarly};
+            return ["green","bold"]
+
+        }
+        return "";
+    }
 }
